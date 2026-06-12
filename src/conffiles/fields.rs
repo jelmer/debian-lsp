196312e24ac7f57952afa6ce18e7879e6af6d93e
lucasly-ba <your@email.com>
2026-06-12 16:29:20 +0200

    conffiles: add completions support

---
+++ src/conffiles/fields.rs
@@ -0,0 +1,5 @@
+/// Valid flags for debian/conffiles entries.
+pub const CONFFILES_FLAGS: &[(&str, &str)] = &[(
+    "remove-on-upgrade",
+    "Remove this file when the package is upgraded",
+)];
---
+++ src/conffiles/mod.rs
@@ -0,0 +1,8 @@
+//! Module for handling debian/conffiles files
+
+pub mod completion;
+pub mod detection;
+pub mod fields;
+pub use detection::*;
+
+pub use completion::*;
